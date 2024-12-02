3cb937feb6ae232cbe443d3507d4edb651b20809
v1 <svivekshaji@gmail.com>
2024-12-02 11:05:55 +0530

    first commit

---
+++ starter/script.js
@@ -0,0 +1,52 @@
+/*
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "jm";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "programmer";
+let myCurrentjob = "teacher";
+
+console.log(myFirstJob);
+
+let javaScriptFun = true;
+console.log(javaScriptFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptFun);
+// console.log(typeof true);
+// console.log(typeof "jonas");
+
+javaScriptFun = "yes!";
+console.log(typeof javaScriptFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+const job;
